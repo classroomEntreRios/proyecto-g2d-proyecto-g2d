2ace74d6d736f07e4079ed66339cf4550d0a849f
veros7821 <veros7821@gmail.com>
2021-03-18 20:16:39 -0300

    cambios

--- viajar360/app/src/app/app.module.ts
+++ viajar360/app/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import {RegistroComponent} from './component/registro/registro.component';
+import {BodyComponent} from './component/body/body.component';
+import {FooterComponent} from './component/footer/footer.component';
+import {NavbarComponent} from './component/navbar/navbar.component';
+import { ComponentComponent } from './component/component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroComponent,
+    FooterComponent,
+    NavbarComponent,
+    BodyComponent,
+    ComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
