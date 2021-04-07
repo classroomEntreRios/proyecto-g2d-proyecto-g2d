57f1c700beeef0c0d62b8ff2e44f163e885ea611
veros7821 <veros7821@gmail.com>
2021-04-06 21:00:37 -0300

    api clima mejorada -

--- Viajar360/viajar360FrontEnd/src/app/inicio/inicio.component.ts
+++ Viajar360/viajar360FrontEnd/src/app/inicio/inicio.component.ts
@@ -9,16 +9,25 @@ import { WheaterService } from '../services/wheater.service';
 export class InicioComponent implements OnInit {
 
   title = 'Viajar360FrontEnd';
-  clima;
+  clima: any;
   constructor(private weatherService: WheaterService) {
 
   }
 
   ngOnInit() {
+   
+    this.clima = {
+      main : {},
+      isDay: true
+    };
     
+    this.getWeatherData();
+    console.log(this.clima);
   }
 
   getWeather(ciudad: string, codigo:  string) {
+    codigo = 'Ar';
+ 
     this.weatherService.getWeather(ciudad, codigo)
     .subscribe (
       res => { 
@@ -27,12 +36,36 @@ export class InicioComponent implements OnInit {
     },
       err => console.log(err)
     )
+  
+ }
+
+  
+  
+
+  getWeatherData(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Paraná,ar&appid=ff1bc4683fc7325e9c57e586c20cc03e')
+    .then(response=>response.json())
+    .then(data=>{this.setWeatherData(data);})
   }
+
+  setWeatherData(data){
+    this.clima = data;
+    let sunsetTime = new Date(this.clima.sys.sunset * 1000);
+    this.clima.sunset_time = sunsetTime.toLocaleTimeString();
+    let currentDate = new Date();
+    this.clima.isDay = (currentDate.getTime() < sunsetTime.getTime());
+    this.clima.temp_celcius = (this.clima.main.temp);
+    this.clima.temp_min = (this.clima.main.temp_min);
+    this.clima.temp_max = (this.clima.main.temp_max);
+    
+  }
+
   submitciudad(ciudad: HTMLInputElement, codigo: HTMLInputElement) {
+    codigo.value = 'Ar';
     if (ciudad.value && codigo.value) {
       this.getWeather(ciudad.value, codigo.value);
       ciudad.value = '';
-      codigo.value = '';
+     
     } else {
       alert ('Por favor, ingrese datos válidos');
     }
